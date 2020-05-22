1778a291c44a3d6c651fb430c760a04e915df73d
Maxel <gigaabait@yandex.ru>
2020-05-22 23:41:52 +0400

    some adative

---
+++ build/precache-manifest.28a4a07a02469cfd6101a8e6bf7a7d81.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f95cf3e08b82236dfff7d151f8664ab",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "7134534f968f4b14797e",
+    "url": "/test-task/static/css/main.2525096f.chunk.css"
+  },
+  {
+    "revision": "115610bc6fb363d6f12b",
+    "url": "/test-task/static/js/2.66e2656b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-task/static/js/2.66e2656b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7134534f968f4b14797e",
+    "url": "/test-task/static/js/main.9049b8f9.chunk.js"
+  },
+  {
+    "revision": "df15afeac7e4922fd30b",
+    "url": "/test-task/static/js/runtime-main.fd73edcb.js"
+  },
+  {
+    "revision": "2af6a67fa9b2e2f8fb48c851c126cfdd",
+    "url": "/test-task/static/media/bounty.2af6a67f.png"
+  },
+  {
+    "revision": "39d42dc35e982f82d8cd2425ac0aba50",
+    "url": "/test-task/static/media/cake.39d42dc3.png"
+  },
+  {
+    "revision": "0c6d4344cd53cee02a25a36e0d8ed250",
+    "url": "/test-task/static/media/coca.0c6d4344.png"
+  },
+  {
+    "revision": "854009cc18112df0ec365e22ab9261e8",
+    "url": "/test-task/static/media/coffee.854009cc.png"
+  },
+  {
+    "revision": "19785165c86a8f4340b3eb83709d1957",
+    "url": "/test-task/static/media/cross.19785165.jpg"
+  },
+  {
+    "revision": "c9bb9f5e8358504644dced1433a4f984",
+    "url": "/test-task/static/media/mars.c9bb9f5e.png"
+  },
+  {
+    "revision": "c852d45e6e653a323090c7541c0d296d",
+    "url": "/test-task/static/media/orbit.c852d45e.png"
+  },
+  {
+    "revision": "54c8cdfe989391e669da42f59f496d65",
+    "url": "/test-task/static/media/snickers.54c8cdfe.png"
+  },
+  {
+    "revision": "1ea3881b1aec00ba2a2ddb4c50996edd",
+    "url": "/test-task/static/media/sprite.1ea3881b.png"
+  },
+  {
+    "revision": "97daeb1b4b41395d3d0710758d515bb2",
+    "url": "/test-task/static/media/tea.97daeb1b.png"
+  }
+]);
